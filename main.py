1b0bb73343de1bc4f7a97ab838c45f94dd01d680
Abdessalem Tebbikh <abdessalem.tebbikh@gmail.com>
2025-03-28 17:54:02 +0100

    update the usage of api-finance

--- main.py
+++ main.py
@@ -2,43 +2,16 @@ import asyncio
 from websockets.asyncio.client import connect
 from websockets import ConnectionClosed
 
-async def connect_to_data_stream(symbol : str):
+async def get_data_stream(symbols : str):
     # using the api-finance project in my git repos
-    async with connect(f"ws://127.0.0.1:8000/ws/{symbol}") as websocket:
+    async with connect(f"ws://127.0.0.1:8000/ws?symbols={symbols}") as websocket:
         while True:
             try:
                 message = await websocket.recv()
-                print(f"{symbol=} received {message=}")
+                print(f"received {message=}")
             except ConnectionClosed:
                 print("Connection closed")
                 break
 
-async def main():
-    symbols = [
-        "AAPL",
-        "IBM",
-        "MSFT",
-        "GOOGL",
-        "AMZN",
-        "TSLA",
-        "NVDA",
-        "JPM",
-        "JNJ",
-        "V",
-        "PG",
-        "XOM",
-        "CVX",
-        "WMT",
-        "HD",
-        "MA",
-        "DIS",
-        "PFE",
-        "BAC"
-    ]
-
-    tasks = [connect_to_data_stream(symbol) for symbol in symbols]
-
-    await asyncio.gather(*tasks)
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(get_data_stream("AAPL,IBM,MSFT"))
